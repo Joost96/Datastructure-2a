794eda26bb5bb98290d164b40fa610a6bc7a7fd4
Joost96 <joost-k@live.nl>
2014-09-25 10:25:59 +0200

    student flile is filled

--- Datastrucutre-2a/src/datastrucutre2a/models/Students.java
+++ Datastrucutre-2a/src/datastrucutre2a/models/Students.java
@@ -12,4 +12,39 @@ package datastrucutre2a.models;
  */
 public class Students {
     
+    private int studentNr;
+    private String group;
+    private float grade;
+    
+    public Students(int studentNr , String group , float grade) {
+        this.studentNr = studentNr;
+        this.group = group;
+        this.grade = grade;
+    }
+
+    public int getStudentNr() {
+        return studentNr;
+    }
+
+    public void setStudentNr(int studentNr) {
+        this.studentNr = studentNr;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+    
+    
 }
